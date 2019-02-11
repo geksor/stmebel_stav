375a24c8b3bc861f1a5572b81b38ee60461d8d49
Geksor <geksor@gmail.com>
2019-02-11 09:45:58 +0300

    test

--- frontend/views/catalog/price.php
+++ frontend/views/catalog/price.php
@@ -7,9 +7,9 @@
 ?>
 
 <? if ($model->sale) {?>
-    <p class="product_right_price_1 main-price" data-price="<?= $model->getSaleCalcPrice($model->productAttrs) ?>" ><?= $model->getSaleCalcPrice($model->productAttrs, true) ?> Р</p>
-    <p class="product_right_price_2"><?= $model->getCalcPrice($model->productAttrs, true) ?> Р</p>
+    <p class="product_right_price_1 main-price" data-price="<?= $model->getSaleCalcPrice($model->productAttrs) ?>" ><?= $model->getSaleCalcPrice($model->productAttrs, true) ?> <i class="fas fa-ruble-sign"></i></p>
+    <p class="product_right_price_2"><?= $model->getCalcPrice($model->productAttrs, true) ?> <i class="fas fa-ruble-sign"></i></p>
 <?}else{?>
-    <p class="product_right_price_1 main-price" data-price="<?= $model->getCalcPrice($model->productAttrs) ?>"><?= $model->getCalcPrice($model->productAttrs, true) ?> Р</p>
+    <p class="product_right_price_1 main-price" data-price="<?= $model->getCalcPrice($model->productAttrs) ?>"><?= $model->getCalcPrice($model->productAttrs, true) ?> <i class="fas fa-ruble-sign"></i></p>
 <?}?>
 
