ecdd819f47b204afb54fbc64b0c426d80418a10a
Geksor <geksor@gmail.com>
2019-02-11 11:42:53 +0300

    test

--- frontend/controllers/CartController.php
+++ frontend/controllers/CartController.php
@@ -142,9 +142,11 @@ class CartController extends Controller
 
         if (empty($cartProduct)){
             Yii::$app->session->setFlash('warning', 'Корзина пуста');
-            if (Yii::$app->request->pathInfo === 'cart'){
+
+            if (Yii::$app->request->referrer === Yii::$app->request->absoluteUrl){
                 return $this->redirect('/');
             }
+
             return $this->redirect(Yii::$app->request->referrer);
         }
 
