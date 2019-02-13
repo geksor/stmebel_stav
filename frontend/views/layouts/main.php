307dc10b853951045bd7998b3191a3a2f260e045
Geksor <geksor@gmail.com>
2019-02-13 15:52:03 +0300

    Ya metrica add

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -197,9 +197,9 @@ $js = <<< JS
     });
     
    window.replainSettings = { id: '2085b6b3-80d5-4e90-9721-8955cca4c24b' };
-(function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;
-var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
-})('https://widget.replain.cc/dist/client.js');
+    (function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;
+    var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+    })('https://widget.replain.cc/dist/client.js');
 
  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(25330049, "init", { id:25330049, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); 
 JS;
