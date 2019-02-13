da815402cb7972e4f946a06da05241b3ac36fadb
Geksor <geksor@gmail.com>
2019-02-13 15:14:33 +0300

    Ya metrica add

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -195,6 +195,18 @@ $js = <<< JS
             }
         }
     });
+    
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(52218184, "init", {
+        id:52218184,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
 JS;
 
 $this->registerJs($js, $position = yii\web\View::POS_END, $key = null);
