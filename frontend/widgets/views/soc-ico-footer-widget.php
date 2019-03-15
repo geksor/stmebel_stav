20b76588e9bded8de93eecd5472cdd4efcfb36cb
Geksor <geksor@gmail.com>
2019-03-15 15:34:56 +0300

    Ya metrica add

--- frontend/widgets/views/soc-ico-footer-widget.php
+++ frontend/widgets/views/soc-ico-footer-widget.php
@@ -6,17 +6,17 @@
 
 <div class="socicoWrapFromFooter" style="display: flex; flex-wrap: nowrap">
     <div  style="flex: 1; padding: 10px">
-        <a href="<?= $model->insta ?>" class="socicolinkFromFooter" style="display: block; font-size: x-large">
+        <a href="<?= $model->insta ?>" target="_blank" class="socicolinkFromFooter" style="display: block; font-size: x-large">
             <img src="/public/img/insta.png" alt="insta.png">
         </a>
     </div>
     <div  style="flex: 1; padding: 10px">
-        <a href="<?= $model->whatsApp ?>" class="socicolinkFromFooter" style="display: block; font-size: x-large">
+        <a href="https://wa.me/<?= $model->whatsApp ?>/?text=Здравствуйте%20мне%20необходимо%20консультация%20по%20Вашим%20услугам" target="_blank" class="socicolinkFromFooter" style="display: block; font-size: x-large">
             <img src="/public/img/watsapp.png" alt="insta.png">
         </a>
     </div>
     <div  style="flex: 1; padding: 10px">
-        <a href="<?= $model->telegram ?>" class="socicolinkFromFooter" style="display: block; font-size: x-large">
+        <a href="tg://resolve?domain=<?= $model->telegram ?>" target="_blank" class="socicolinkFromFooter" style="display: block; font-size: x-large">
             <img src="/public/img/telegramm.png" alt="telegramm.png">
         </a>
     </div>
